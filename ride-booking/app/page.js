bfcd6fb0c8a607a005ffc11ec3da761cc7c1acda
InshiyaRavat <insravat@gmail.com>
2024-12-07 14:58:13 +0530

    Update : adjusted layout

--- ride-booking/app/page.js
+++ ride-booking/app/page.js
@@ -3,8 +3,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <UserButton/>
+    <div className='p-6 grid grid-cols-1 md:grid-cols-3 gap-5'>
+      <div>
+        search
+      </div>
+      <div className="col-span-2">
+        google map
+      </div>
     </div>
   );
 }
