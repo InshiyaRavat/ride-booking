a7be643a4534b3c118dd68c27bcb467ea80ffd96
InshiyaRavat <insravat@gmail.com>
2024-12-12 22:04:56 +0530

    Update : added users location as center and marker position

--- ride-booking/components/Home/Map/LeafletMap.js
+++ ride-booking/components/Home/Map/LeafletMap.js
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
@@ -8,8 +8,25 @@ import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 
 const centerPostion = [51.505, -0.09]
 const markerPosition = [51.505, -0.09]
-
 const LeafletMap = () => {
+    const [location, setLocation] = useState(null)
+
+    useEffect(()=>{
+        if(!navigator.geolocation){
+            console.log("Geolocation is not supported by your browser.")
+        }
+        else{
+            navigator.geolocation.getCurrentPosition((position)=>{
+                const {latitude, longitude} = position.coords
+                setLocation({latitude, longitude})
+            })
+        }
+    },[])
+    if(location){
+        centerPostion = [location.latitude, location.longitude]
+        markerPosition = [location.latitude, location.longitude]
+    }
+    
   return (
     <div className='h-[400px] w-full'>
         <MapContainer
